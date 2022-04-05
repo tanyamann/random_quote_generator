9e85ad76ccab4fa2b16a8854c99af71bb863e219
Tanya Mann <thombre99@protomail.com>
2022-04-04 18:58:31 -0600

    Zerihun code

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 // console.log('connected!')
 //const url="https://type.fit/api/quotes" 
 
-
 // const button = document.getElementById('button');
-
-showQuote=()=>{
-    let displayFact = document.querySelector(".display-facts")
-    let url = "https://type.fit/api/quotes";
-}
-fetch(url) 
-    .then(response =>{
-      return response.json()
-    })
-    .then(responseData => {
-        console.log("response", responseData)
-        displayFact.innerHTML = responseData.value;
-    })
+// Fetching the quotes from the type.fit API using promises
+// showQuote=()=>{
+//         let displayQuote = document.querySelector(".displayQuote")
+fetch("https://type.fit/api/quotes")
+    .then(function(response) {
+        return response.json(); 
+    }) // Getting the raw JSON data
+    .then(function(data) {
+        // Storing the quotes internally upon 
+        // successful completion of request
+        this.data = data; 
+    // }) // Setting
+        // Displaying the quote When the Webpage loads
+        displayQuote() 
     
-}
+});
+// Adding an onclick listener for the button
+ 
+// Adding an onclick listener for the button
+// function newQuote(){
+// showQuote=()=>{
+//     let displayQuote = document.querySelector(".displayQuote")
+//     let url = "https://type.fit/api/quotes";
+
+// fetch(url) 
+//     .then(response =>{
+//       return response.json()
+//     })
+//     .then(responseData => {
+//         // console.log("response", responseData)
+//         displayQuote.innerHTML = responseData.value;
+//     })
+    
+// }
